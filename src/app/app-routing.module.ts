a363523e0241cf178b0cbceaa8eba9ff2bdc7fe5
Esteban Boucher <es.boucher@protomail.com>
2020-09-25 05:03:06 +0200

    Managed to do first nested routes for login / sign up in home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,9 +6,18 @@ import { RegisterComponent } from './register';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: '', component: HomeComponent },
+  { path: '', component: HomeComponent,
+    children: [
+      {
+        path: '',
+        component: LoginComponent,
+      },
+      {
+        path: 'register',
+        component: RegisterComponent,
+      },
+    ]
+  }
 ];
 
 @NgModule({
