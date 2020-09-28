ca42209e39e8771548ea712f8096b107cc077073
Esteban Boucher <es.boucher@protomail.com>
2020-09-28 03:54:08 +0200

    Added contact form template

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+
+  contactForm = new FormGroup({
+  });
 
   constructor() { }
 
