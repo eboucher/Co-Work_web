05293b4fa6c30488d19c17e5ab43af038d1f5191
Esteban Boucher <es.boucher@protomail.com>
2020-09-29 02:55:52 +0200

    Starting to get data from fake db (json) instead of hard-coding it

--- src/app/locations/locations.component.ts
+++ src/app/locations/locations.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
+import * as data from "/home/esteban/Documents/ESGI/2-PA_R/Co-Work_web/db.json";
+
 @Component({
   selector: 'app-locations',
   templateUrl: './locations.component.html',
   styleUrls: ['./locations.component.scss']
 })
 export class LocationsComponent {
+
+  locations : any[] = data.locations;
 
   profileForm = new FormGroup({
   });
