a14f3fa97329b22885d27abd5890928468146363
Esteban Boucher <es.boucher@protomail.com>
2020-10-01 02:59:51 +0200

    Fixed booking templates by adding child routes

--- src/app/booking/booking-routing.module.ts
+++ src/app/booking/booking-routing.module.ts
@@ -7,10 +7,14 @@ import { SecondStepComponent } from './second-step';
 import { ThirdStepComponent } from './third-step';
 
 const bookingRoutes: Routes = [
-  { path: 'book',  component: BookComponent, data: { animation: 'book' } },
-  { path: 'book/first-step', component: FirstStepComponent, data: { animation: 'first-step' } },
-  { path: 'book/second-step', component: SecondStepComponent, data: { animation: 'second-step' } },
-  { path: 'book/third-step', component: ThirdStepComponent, data: { animation: 'third-step' } }
+  { path: 'book',  component: BookComponent,
+    data: { animation: 'book' },
+    children: [
+      { path: 'first-step', component: FirstStepComponent, data: { animation: 'first-step' } },
+      { path: 'second-step', component: SecondStepComponent, data: { animation: 'second-step' } },
+      { path: 'third-step', component: ThirdStepComponent, data: { animation: 'third-step' } },
+    ] 
+  }
 ];
 
 @NgModule({
