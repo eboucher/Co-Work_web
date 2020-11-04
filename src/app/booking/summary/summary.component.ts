3065b62afff63c2121580cf7d26db997cab093fc
Esteban Boucher <es.boucher@protomail.com>
2020-11-04 17:28:24 +0100

    Summary sort of sufficient for now

--- src/app/booking/summary/summary.component.ts
+++ src/app/booking/summary/summary.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { LocationService } from '@app/locations/location.service';
+import { Booking } from '@app/_models';
+import { BookingService } from '../booking.service';
 
 @Component({
   selector: 'app-summary',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SummaryComponent implements OnInit {
 
-  constructor() { }
+  booking: Booking;
+  location: any;
+
+  constructor(
+    private bookingService: BookingService,
+    private locationService: LocationService) { }
 
   ngOnInit(): void {
+    this.location = this.locationService.location;
+    this.bookingService.currentBooking.subscribe(booking => this.booking = booking);
   }
 
 }
