4d980fc012b45443655161cdea38b60bc1e4c5f0
Esteban Boucher <es.boucher@protomail.com>
2020-11-05 09:14:58 +0100

    Location detail fixed, now retrieving data from WorkspaceService instead of stupid Observable

--- src/app/_services/workspace.service.ts
+++ src/app/_services/workspace.service.ts
@@ -2,13 +2,21 @@ import { Injectable } from '@angular/core';
 import { environment } from '@environments/environment';
 import { map } from 'rxjs/internal/operators';
 import { HttpClient } from '@angular/common/http';
+import { Workspace } from '@app/_models';
 
 @Injectable({
   providedIn: 'root'
 })
-export class OpenSpaceService {
+export class WorkspaceService {
 
   constructor(private http: HttpClient) { }
+
+  getWorkspaceByID(id: string) {
+      return this.http.get<Workspace>(`${environment.apiUrl}/workspaces/${id}`)
+      .pipe(map(resp => {
+        return resp;
+      }));
+  }
 
   book(): void { }
 
--- src/app/locations/location-detail/location-detail.component.ts
+++ src/app/locations/location-detail/location-detail.component.ts
@@ -5,6 +5,8 @@ import { Observable } from 'rxjs';
 
 import { LocationService } from '../location.service';
 import { Workspace } from '@app/_models/workspace';
+import { Room } from '@app/_models';
+import { WorkspaceService } from '@app/_services/workspace.service';
 
 @Component({
   selector: 'app-location-detail',
@@ -14,22 +16,48 @@ import { Workspace } from '@app/_models/workspace';
 export class LocationDetailComponent implements OnInit {
   location$: Observable<Workspace>;
 
+  workspaceID: string;
+  workspace: Workspace;
+  noMeetingRooms: number;
+  noCallRooms: number;
+  noCozyLounges: number;
+  rooms: Room[];
+
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private service: LocationService
+    private service: LocationService,
+    public workspaceService: WorkspaceService
   ) {}
 
 
   ngOnInit() {
+    this.noMeetingRooms = 0;
+    this.noCallRooms = 0;
+    this.noCozyLounges = 0;
     this.location$ = this.route.paramMap.pipe(
       switchMap((params: ParamMap) =>
         this.service.getLocation(params.get('id')))
     );
-    //console.log("this.location$ = " + this.location$);
-  }
 
-  gotoLocations() {
-    this.router.navigate(['/locations']);
+    this.route.params.subscribe(params => {
+      console.log('The id of this route is: ', params.id);
+      this.workspaceID = params.id;
+    });
+    this.workspaceService.getWorkspaceByID(this.workspaceID).subscribe(e => {
+        console.log("RÉPONSE e = " + e),
+        this.workspace = e,
+        console.log("this.workspace = " + this.workspace.rooms[0].roomType),
+
+        this.workspace.rooms.forEach(room => {
+          console.log("room = " + JSON.stringify(room))
+          if(room.roomType == "meetingRoom")
+            this.noMeetingRooms += 1;
+          if(room.roomType == "callRoom")
+            this.noCallRooms += 1;
+          if(room.roomType == "cozyLouge")
+            this.noCozyLounges += 1;
+        });
+      });
   }
 }
